821d92415a9e2c6bcddb3657ef0d25c49dfefee9
samous <546396456@qq.com>
2019-06-03 20:23:50 +0800

    修改备注

--- src/com/woniu/test/Test.java
+++ src/com/woniu/test/Test.java
@@ -6,7 +6,7 @@ import com.woniu.dao.DAOFactory;
 public class Test {
 
 	public static void main(String[] args) {
-		// 测试类
+		// 法国恢复规划法国恢复规划法规
 		User user = new User(3,"11","3113",2);
 		DAOFactory.getUserDAO().save(user);
 	}
